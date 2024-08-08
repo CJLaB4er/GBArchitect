7a928fca380852bd5957c270ccab8272baa70556
Sinkevich Bronislav <SinkevichBI@gmail.com>
2024-08-09 00:54:26 +0300

    add Poligon.java

---
+++ GBArchitect/src/main/java/ru/geekbrains/architect/lesson1/modelElements/Poligon.java
@@ -0,0 +1,13 @@
+package main.java.ru.geekbrains.architect.lesson1.modelElements;
+
+import java.awt.*;
+import java.util.List;
+
+public class Poligon {
+
+    private List<Point> points;
+
+    public Poligon(List<Point> points) {
+        this.points = points;
+    }
+}
