a239f8c5c1576d2393c6a0a306657d237aa2e8f7
Sinkevich Bronislav <SinkevichBI@gmail.com>
2024-08-09 01:01:59 +0300

    modify PoligonModel.java add two constructors, add getters

--- GBArchitect/src/main/java/ru/geekbrains/architect/lesson1/modelElements/PoligonModel.java
+++ GBArchitect/src/main/java/ru/geekbrains/architect/lesson1/modelElements/PoligonModel.java
@@ -5,4 +5,21 @@ import java.util.List;
 public class PoligonModel {
     private List<Poligon> poligons;
     private List<Texture> textures;
+
+    public PoligonModel(List<Poligon> poligons) {
+        this.poligons = poligons;
+    }
+
+    public PoligonModel(List<Poligon> poligons, List<Texture> textures) {
+        this.poligons = poligons;
+        this.textures = textures;
+    }
+
+    public List<Poligon> getPoligons() {
+        return poligons;
+    }
+
+    public List<Texture> getTextures() {
+        return textures;
+    }
 }
